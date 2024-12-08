83a65178bb92b1d9a869fa9792446bb94b2ec463
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-12-08 15:41:55 +0000

    Add day 8 part1

---
+++ src/2024/day8/part1.ts
@@ -0,0 +1,80 @@
+import fs from "fs";
+
+const input: string = fs.readFileSync("./src/2024/day8/input", {
+  encoding: "utf-8",
+});
+
+const lines: string[] = input.split("\n");
+const matrix: string[][] = lines.map(line => line.split(""));
+
+const printMaze = (maze: string[][]) => {
+  for (const line of maze) {
+    console.log(line.join(""));
+  }
+  console.log("\n");
+};
+const printAntiNodes = (antinodes: Set<string>, maze: string[][]) => {
+  const solvedMaze = JSON.parse(JSON.stringify(maze));
+
+  antinodes.forEach(location => {
+    const [x, y] = location.split(",");
+    solvedMaze[y][x] = "#";
+  });
+
+  printMaze(solvedMaze);
+};
+
+const getCharLocations = (): { [char: string]: string[] } => {
+  const charLocations: { [char: string]: string[] } = {};
+
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (/[\dA-z]/.test(matrix[y][x])) {
+        const char: string = matrix[y][x];
+
+        if (!charLocations[char]) {
+          charLocations[char] = [];
+        }
+        charLocations[char].push(`${x},${y}`);
+      }
+    }
+  }
+
+  return charLocations;
+};
+
+const part1 = () => {
+  const antinodes: Set<string> = new Set<string>();
+  const charLocations: { [char: string]: string[] } = getCharLocations();
+
+  const arrayOfLocations = Object.values(charLocations);
+
+  const basicMaze: string[][] = matrix;
+
+  for (const locations of arrayOfLocations) {
+    locations.forEach((location, index) => {
+      let startingIndex = 0;
+
+      locations.forEach(newLocation => {
+        if (index !== startingIndex) {
+          const [sX, sY] = location.split(",").map(Number);
+          const [lX, lY] = newLocation.split(",").map(Number);
+
+          const deltaX = lX - sX;
+          const deltaY = lY - sY;
+
+          if (basicMaze[sY - deltaY]?.[sX - deltaX]) {
+            antinodes.add(`${sX - deltaX},${sY - deltaY}`);
+          }
+          if (basicMaze[lY + deltaY]?.[lX + deltaX]) {
+            antinodes.add(`${lX + deltaX},${lY + deltaY}`);
+          }
+        }
+        startingIndex++;
+      });
+    });
+  }
+  console.log(antinodes.size);
+};
+
+part1();
