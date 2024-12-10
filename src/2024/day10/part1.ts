6e5e21c12f36f67fe65f75c8152cbb417ef7b296
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-12-10 22:04:14 +0000

    Add recursive day 10 part1

--- src/2024/day10/part1.ts
+++ src/2024/day10/part1.ts
@@ -7,6 +7,13 @@ const input: string = fs.readFileSync("./src/2024/day10/input", {
 const lines: string[] = input.split("\n");
 const matrix: string[][] = lines.map(line => line.split(""));
 
+const moves = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
 const getTrailheads = (): string[][] => {
   const positions: string[][] = [];
   for (let y = 0; y < matrix.length; y++) {
@@ -39,22 +46,82 @@ const part1 = () => {
         continue;
       }
 
-      if (Number(matrix[y][x]) + 1 === Number(matrix[y + 1]?.[x])) {
-        toVisit.push([x, y + 1]);
+      for (const [moveX, moveY] of moves) {
+        if (
+          Number(matrix[y][x]) + 1 ===
+          Number(matrix[y + moveY]?.[x + moveX])
+        ) {
+          toVisit.push([x + moveX, y + moveY]);
+        }
       }
-      if (Number(matrix[y][x]) + 1 === Number(matrix[y - 1]?.[x])) {
-        toVisit.push([x, y - 1]);
-      }
-      if (Number(matrix[y][x]) + 1 === Number(matrix[y]?.[x + 1])) {
-        toVisit.push([x + 1, y]);
-      }
-      if (Number(matrix[y][x]) + 1 === Number(matrix[y]?.[x - 1])) {
-        toVisit.push([x - 1, y]);
-      }
+
+      // if (Number(matrix[y][x]) + 1 === Number(matrix[y + 1]?.[x])) {
+      //   toVisit.push([x, y + 1]);
+      // }
+      // if (Number(matrix[y][x]) + 1 === Number(matrix[y - 1]?.[x])) {
+      //   toVisit.push([x, y - 1]);
+      // }
+      // if (Number(matrix[y][x]) + 1 === Number(matrix[y]?.[x + 1])) {
+      //   toVisit.push([x + 1, y]);
+      // }
+      // if (Number(matrix[y][x]) + 1 === Number(matrix[y]?.[x - 1])) {
+      //   toVisit.push([x - 1, y]);
+      // }
     }
   }
 
   console.log(total);
 };
 
-part1();
+const hike = (x: number, y: number, visited: Set<string>): number => {
+  if (visited.has(`${x},${y}`)) {
+    return 0;
+  }
+
+  let acum = 0;
+
+  if (matrix[y][x] === "9") {
+    visited.add(`${x},${y}`);
+    acum++;
+  }
+
+  for (const [moveX, moveY] of moves) {
+    if (Number(matrix[y][x]) + 1 === Number(matrix[y + moveY]?.[x + moveX])) {
+      acum += hike(x + moveX, y + moveY, visited);
+    }
+  }
+
+  return acum;
+};
+
+const hike2 = (x: number, y: number, visited: Set<string>): number => {
+  if (!visited.has(`${x},${y}`) && matrix[y][x] === "9") {
+    visited.add(`${x},${y}`);
+    return 1;
+  }
+
+  let acum = 0;
+
+  for (const [moveX, moveY] of moves) {
+    if (Number(matrix[y][x]) + 1 === Number(matrix[y + moveY]?.[x + moveX])) {
+      acum += hike2(x + moveX, y + moveY, visited);
+    }
+  }
+
+  return acum;
+};
+
+const part11 = () => {
+  let total = 0;
+  const trailheads = getTrailheads();
+
+  for (let [startX, startY] of trailheads) {
+    const visited: Set<string> = new Set();
+
+    total += hike2(Number(startX), Number(startY), visited);
+  }
+
+  console.log(total);
+};
+
+part11();
